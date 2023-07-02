dd5201d36b43064cfa3be7e7c45336bad2421e50
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-07-02 14:38:36 +0100

    got the line for pluto!

--- src/angles.rs
+++ src/angles.rs
@@ -20,41 +20,42 @@ pub fn plot(
         .draw()
         .unwrap();
     let planet = &super::PLANETS[index];
-    // when eccentricity is much less than 1 (in this case zero) t is roughly P * theta
+    // when eccentricity is zero t is roughly P * theta
     chart
         .draw_series(LineSeries::new(
             [0.0, 800.0].into_iter().map(|x| (x, planet.orbit * x)),
             WHITE,
         ))
         .unwrap();
-    // i've got a bit of brain block on this
 
-    // let theta: Vec<_> = (0_f32..20.0)
-    //     .step(0.001)
-    //     .values()
-    //     .map(|theta| (1.0 - planet.eccentricity * theta.cos()).powi(-2))
-    //     .collect();
-    // // multiply by coefficients
-    // let len = theta.len();
-    // chart
-    //     .draw_series(LineSeries::new(
-    //         (0_f32..20.0).step(0.01).values().map(|x| {
-    //             let mut theta = theta[..len - 1 - (x * 1000.0) as usize].to_vec();
-    //             for (i, val) in theta[1..len - 2].iter_mut().enumerate() {
-    //                 *val *= if i % 2 == 1 { 4.0 } else { 2.0 }
-    //             }
-    //             (
-    //                 x,
-    //                 planet.orbit
-    //                     * (1.0 - planet.eccentricity.powi(2)).powf(1.5)
-    //                     * std::f32::consts::FRAC_1_PI
-    //                     / 2.0
-    //                     / 1000.0
-    //                     / 3.0
-    //                     * theta.into_iter().sum::<f32>(),
-    //             )
-    //         }),
-    //         WHITE,
-    //     ))
-    //     .unwrap();
+    let vals: Vec<_> = (0_f32..20.0)
+        .step(0.001)
+        .values()
+        .map(|theta| (1.0 - planet.eccentricity * theta.cos()).powi(-2))
+        .collect();
+    // multiply by coefficients
+    chart
+        .draw_series(LineSeries::new(
+            (0.01_f32..20.0).step(0.01).values().map(|y| {
+                let mut theta = vals[..(y * 1000.0) as usize].to_vec();
+                let len = theta.len();
+                for (i, val) in theta[1..len - 2].iter_mut().enumerate() {
+                    *val *= if i % 2 == 1 { 4.0 } else { 2.0 }
+                }
+                (
+                    planet.orbit
+                        * (1.0 - planet.eccentricity.powi(2)).powf(1.5)
+                        // 1/(2*pi)
+                        * std::f32::consts::FRAC_1_PI
+                        / 2.0
+                        // h/3
+                        * 0.001
+                        / 3.0
+                        * theta.into_iter().sum::<f32>(),
+                    y,
+                )
+            }),
+            WHITE,
+        ))
+        .unwrap();
 }
