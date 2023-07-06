ae5cd41b0f034042b76031be68adcd36ece6939e
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-07-06 15:56:47 +0100

    scale to orbit arc

--- src/spiral.rs
+++ src/spiral.rs
@@ -6,21 +6,29 @@ pub fn plot(
     (i1, i2, lines): &(usize, usize, Vec<[(f32, f32); 2]>),
 ) {
     let (p1, p2) = (&super::PLANETS[*i1], &super::PLANETS[*i2]);
-    let scale = p1.distance.max(p2.distance) / 30.0;
+    let far = match p1.distance > p2.distance {
+        true => p1,
+        false => p2,
+    };
+    // axis bounds are the same as the maximums and minimums of the largest orbit (slightly adjusted)
+    let x = |θ: f32| {
+        let cos = θ.cos();
+        (far.distance * (1.0 - far.eccentricity.powi(2))) / (1.0 - far.eccentricity * cos)
+            * cos
+            * 1.1
+    };
+    let y = |θ: f32| {
+        let (sin, cos) = θ.sin_cos();
+        (far.distance * (1.0 - far.eccentricity.powi(2))) / (1.0 - far.eccentricity * cos)
+            * sin
+            * 1.1
+    };
+
     let mut chart = ChartBuilder::on(root)
-        .build_cartesian_2d(-30.0 * scale..50.0 * scale, -40.0 * scale..40.0 * scale)
-        .unwrap();
-    chart
-        .plotting_area()
-        .draw(&Circle::new(
-            (0.0, 0.0),
-            (1.0 / scale) as i32,
-            ShapeStyle {
-                color: full_palette::AMBER.into(),
-                filled: true,
-                stroke_width: 0,
-            },
-        ))
+        .build_cartesian_2d(
+            x(std::f32::consts::PI)..x(0.0),
+            y(std::f32::consts::PI / 2.0)..y(std::f32::consts::PI / 2.0 * 3.0),
+        )
         .unwrap();
     for line in lines.iter() {
         chart
