6d0156cef3b60e9b50418f6b8caa28edc5cb3ca2
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2023-07-26 09:14:52 +0100

    use macro to reduce verbosity

--- src/orbits.rs
+++ src/orbits.rs
@@ -5,15 +5,12 @@ pub fn plot(
     trans: &egui_plotter::Transform,
     &(scale, time, speed, animated, three_d): &(f32, instant::Instant, f32, bool, bool),
 ) {
-    match three_d {
-        false => {
-            let mut chart = ChartBuilder::on(root)
-                .build_cartesian_2d(-30.0 * scale..50.0 * scale, -40.0 * scale..40.0 * scale)
-                .unwrap();
-            chart
+    macro_rules! draw {
+        ($chart: expr, $centre: expr, $map: ident) => {
+            $chart
                 .plotting_area()
                 .draw(&Circle::new(
-                    (0.0, 0.0),
+                    $centre,
                     (1.0 / scale) as i32,
                     ShapeStyle {
                         color: full_palette::AMBER.into(),
@@ -23,22 +20,22 @@ pub fn plot(
                 ))
                 .unwrap();
             for planet in super::PLANETS.iter() {
-                chart
+                $chart
                     .draw_series(LineSeries::new(
                         (0_f32..361.0)
                             .step(2.5)
                             .values()
-                            .map(|θ| planet.coord(θ.to_radians())),
+                            .map(|θ| planet.$map(θ.to_radians())),
                         planet.colour,
                     ))
                     .unwrap();
                 if animated {
                     let rad = planet.radius.max(1.0);
-                    chart
+                    $chart
                         .plotting_area()
                         .draw(
                             &(EmptyElement::at(
-                                planet.coord(planet.angle(time.elapsed().as_secs_f32() * speed)),
+                                planet.$map(planet.angle(time.elapsed().as_secs_f32() * speed)),
                             ) + Circle::new(
                                 (0, 0),
                                 rad,
@@ -65,6 +62,15 @@ pub fn plot(
                         .unwrap();
                 }
             }
+        };
+    }
+
+    match three_d {
+        false => {
+            let mut chart = ChartBuilder::on(root)
+                .build_cartesian_2d(-30.0 * scale..50.0 * scale, -40.0 * scale..40.0 * scale)
+                .unwrap();
+            draw!(chart, (0.0, 0.0), coord);
         }
         true => {
             let mut chart = ChartBuilder::on(root)
@@ -79,61 +85,7 @@ pub fn plot(
                 pb.pitch = trans.pitch;
                 pb.into_matrix()
             });
-            chart
-                .plotting_area()
-                .draw(&Circle::new(
-                    (0.0, 0.0, 0.0),
-                    (1.0 / scale) as i32,
-                    ShapeStyle {
-                        color: full_palette::AMBER.into(),
-                        filled: true,
-                        stroke_width: 0,
-                    },
-                ))
-                .unwrap();
-            for planet in super::PLANETS.iter() {
-                chart
-                    .draw_series(LineSeries::new(
-                        (0_f32..361.0)
-                            .step(2.5)
-                            .values()
-                            .map(|θ| planet.position(θ.to_radians())),
-                        planet.colour,
-                    ))
-                    .unwrap();
-                if animated {
-                    let rad = planet.radius.max(1.0);
-                    chart
-                        .plotting_area()
-                        .draw(
-                            &(EmptyElement::at(
-                                planet.position(planet.angle(time.elapsed().as_secs_f32() * speed)),
-                            ) + Circle::new(
-                                (0, 0),
-                                rad,
-                                ShapeStyle {
-                                    color: planet.colour.into(),
-                                    filled: true,
-                                    stroke_width: 0,
-                                },
-                            ) + Circle::new(
-                                (0, 0),
-                                rad,
-                                ShapeStyle {
-                                    color: WHITE.into(),
-                                    filled: false,
-                                    stroke_width: 1,
-                                },
-                            ) + Text::new(
-                                planet.name,
-                                (10, -10),
-                                FontDesc::new(FontFamily::Serif, 15.0, FontStyle::Normal)
-                                    .color(&WHITE),
-                            )),
-                        )
-                        .unwrap();
-                }
-            }
+            draw!(chart, (0.0, 0.0, 0.0), position);
         }
     };
 }
